151393d9031274d8604b93928f95e1930df98c4f
Drigo <rodrigoaguayo94@gmail.com>
2021-08-31 15:59:58 -0700

    added mutation and queries in client, also added build

--- client/src/utils/mutation.js
+++ client/src/utils/mutation.js
@@ -1,1 +1,61 @@
 import { gql } from '@apollo/client'
+
+export const LOGIN_USER = gql`
+    mutation login ($email: String!, $password: String!){
+        login(email: $email, password: $password){
+            token
+            user{
+                _id
+                username
+                email
+            }
+        }
+    }
+`;
+
+export const ADD_USER = gql`
+    mutation addUser($username: String!, $email: String!, $password: String!) {
+        addUser(username: $username, email: $email, password: $password){
+            token
+            user {
+                _id
+                username
+                email
+            }
+        }
+    }
+`
+
+export const SAVE_BOOK = gql`
+mutation saveBook($bookToSave: BookInput) {
+    saveBook(bookToSave: $bookToSave) {
+        _id
+        username
+        bookCount
+        savedBooks{
+            bookId
+            authors
+            description
+            title
+            image
+            link
+        }
+    }
+}`;
+
+export const REMOVE_BOOK = gql`
+mutation removeBook($bookId:ID!){
+    removeBook(bookId: $bookId){
+        _id
+        username
+        bookCount
+        savedBooks{
+            bookId
+            authors
+            description
+            title
+            image
+            link
+        }
+    }
+}`
